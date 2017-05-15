bc0f44a2ba065a62c248b83685c5db3fe2563bd0
Anwesh43 <anweshthecool0@gmail.com>
2017-05-15 11:02:10 +0530

    Using FilterRevalView in app

--- app/src/main/java/com/anwesome/ui/conerfilterrevalviewdemo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/conerfilterrevalviewdemo/MainActivity.java
@@ -1,13 +1,19 @@
 package com.anwesome.ui.conerfilterrevalviewdemo;
 
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import com.anwesome.ui.cornerfilterrevealview.FilterRevealView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private FilterRevealView filterRevealView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        filterRevealView = new FilterRevealView(this,BitmapFactory.decodeResource(getResources(),R.drawable.forest1));
         setContentView(R.layout.activity_main);
+        addContentView(filterRevealView,new ViewGroup.LayoutParams(300,300));
     }
 }
